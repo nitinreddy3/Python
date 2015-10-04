fa533a08b17e16bbba4e923c34cf64761221a2f4
Nitin Reddy <reddynitin3@gmail.com>
2015-10-04 23:46:58 +0530

    delete file

--- treehouse/basics-python/lumberjack.py
+++
@@ -1,8 +0,0 @@
-# storing a input accepted from a user in a variable
-name = input("What's your name?: ")
-
-#checking the name entered and executing the code based on the conditions
-if name == "Nitin":
-    print("{} is a lumberjack and he/she's ok".format(name))
-else:
-    print("{} sleeps all night and {} works all day!".format(name, name))

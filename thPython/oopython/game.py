3f5b2bd8411e555e8d4fa05c30671928a5853b54
nitin reddy <nitin.reddy@weboniselab.com>
2015-10-07 12:55:13 +0530

    Added logic for combat module

--- thPython/oopython/game.py
+++ thPython/oopython/game.py
@@ -12,9 +12,13 @@ class Game:
             Troll(),
             Dragon()
         ]
+        self.monster = self.get_next_monster()
 
     def get_next_monster(self):
         try:
             return self.monsters.pop(0)
         except IndexError:
             return None
+
+    def monster_turn(self):
+        

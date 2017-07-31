158884f194a10b1c01752038618bff6c6f58ff20
Nitin Reddy <nitin.reddy@weboniselab.com>
2017-07-31 18:16:31 +0530

    Working example completed on string manipulation

---
+++ lpthw/ex7.py
@@ -0,0 +1,22 @@
+# Creates a variable to store a name
+name = "Nitin Reddy"
+
+# Prints a name on console
+print "My name is %s." % name
+
+# Prints a name on console
+print "Its fleece was white as %s." % 'snow'
+
+print "And everywhere that Mary went."
+
+# Prints '.' for ten times
+print "." * 10
+
+end1 = "B"
+end2 = "u"
+end3 = "r"
+end4 = "g"
+end5 = "e"
+end6 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6

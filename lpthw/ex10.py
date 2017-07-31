b2a79888679e094a99b1928777cc64e7d256bf1c
Nitin Reddy <reddynitin3@gmail.com>
2017-07-31 19:22:11 +0530

    Example on different set of special characters

---
+++ lpthw/ex10.py
@@ -0,0 +1,21 @@
+# String with a tabspace special character
+tabby_cat = "\tI'm tabbed in."
+
+# String with a newline character
+persian_cat = "I'm split\non a line."
+
+# String with a backslash character
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+# Prints the variables
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat

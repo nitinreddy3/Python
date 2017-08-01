d6c63ccf325053b0c069f1b6c7a5d51422ef8719
Nitin Reddy <reddynitin3@gmail.com>
2017-08-01 17:39:01 +0530

    Example on passing parameters to the script and printing the arguments:

---
+++ lpthw/ex13.py
@@ -0,0 +1,11 @@
+# Imports 'argv' from 'sys' module
+from sys import argv
+
+# Assign parameters to the imported argv feature
+script, first, second, third = argv
+
+# Printing the parameters based on accepted arguments 
+print "The script is called: ", script
+print "Your first variable is: ", first
+print "Your second variable is: ", second
+print "Your third variable is: ", third

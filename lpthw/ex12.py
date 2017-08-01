7a4a8711804a50292cc7d29bf1f040223a30117f
Nitin Reddy <reddynitin3@gmail.com>
2017-08-01 17:02:49 +0530

    Example on query and input from user

---
+++ lpthw/ex12.py
@@ -0,0 +1,11 @@
+# Accept age user input along with query
+age = raw_input("How old are you?" )
+
+# Accept height user input along with query
+height = raw_input("How tall are you?" )
+
+# Accept weight user input along with query
+weight = raw_input("How much do you weigh?" )
+
+# Print age, height and weight as per user input
+print "So, you're %r old, %r tall and %r heavy" % (age, height, weight)

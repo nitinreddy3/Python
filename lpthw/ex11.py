f8aa233f4444e981b84cfd3dc3173629fa7d5381
Nitin Reddy <reddynitin3@gmail.com>
2017-08-01 11:34:12 +0530

    Working example on fetching user input and displaying it on console

---
+++ lpthw/ex11.py
@@ -0,0 +1,17 @@
+print "How old are you?"
+
+# Accept raw input as a value for age from user
+age = raw_input()
+
+print "How tall are you?" 
+
+# Accept raw input as a value for height from user
+height = raw_input()
+
+print "How much do you weigh?"
+
+# Accept raw input as a value for weight from user
+weight = raw_input()
+
+# Printing the inputs accepted from user
+print "So, you're %r old, %r tall and %r heavy" % (age, height, weight)

aabd5f1630bf8e8eb22d88e33ebe46bdc0cfaacf
Nitin Reddy <reddynitin3@gmail.com>
2017-08-01 19:02:56 +0530

    Added extra comments for better readability of a code

--- lpthw/ex14.py
+++ lpthw/ex14.py
@@ -3,22 +3,33 @@ from sys import argv
 script, user_name = argv
 prompt = '> '
 
+
+# Accept the values passed as params from terminal
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
+
+# Accept like as yes or no from a user
 likes = raw_input(prompt)
 
+# Condition to execute print statement if a user input is y from previous statement
 if likes == "y":
     print "That's so cute of you!"
 
 print "Where do you live %s?" % user_name
+
+# Accept the residing city of a user
 lives = raw_input(prompt)
 
 if lives == 'pune':
     print "Which country does it belong to ?"
+    
+    # Accept the residing country of a user        
     country = raw_input(prompt)
 
 print "What kind of computer do you have?"
+
+# Accept the type of computer a user uses
 computer = raw_input(prompt)
 
 print """

d5d43957fdd5b8ecde96b398e712feae510f5adc
Nitin Reddy <reddynitin3@gmail.com>
2017-08-03 15:06:45 +0530

    Reading a line by line data from an input line

--- lpthw/function_file.py
+++ lpthw/function_file.py
@@ -10,6 +10,13 @@ def print_all(f):
 def rewind(f):
     f.seek(0)
 
+def print_a_line(line_count, f):
+    print line_count, f.readline()
+
 print_all(current_file)
 
 rewind(current_file)
+
+count_line = 1
+print_a_line(count_line, current_file)
+

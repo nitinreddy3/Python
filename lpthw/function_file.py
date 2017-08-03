94fc8d3802bd454e1d74829cd2419a6701c053f4
Nitin Reddy <reddynitin3@gmail.com>
2017-08-03 15:09:19 +0530

    Added comments for functions

--- lpthw/function_file.py
+++ lpthw/function_file.py
@@ -4,12 +4,14 @@ script, input_file = argv
 
 current_file = open(input_file)
 
+# Function to read all the data in a file
 def print_all(f):
     print f.read()
 
 def rewind(f):
     f.seek(0)
 
+# Function to read data line by line
 def print_a_line(line_count, f):
     print line_count, f.readline()
 
@@ -17,6 +19,7 @@ print_all(current_file)
 
 rewind(current_file)
 
+# Print a first line of data 
 count_line = 1
 print_a_line(count_line, current_file)
 

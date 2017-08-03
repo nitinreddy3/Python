91f93b0a5ce8221c5c2c85b78ea3341acd71c0bc
Nitin Reddy <reddynitin3@gmail.com>
2017-08-03 17:23:41 +0530

    Example to check how functions returns value

---
+++ lpthw/function_return.py
@@ -0,0 +1,24 @@
+# Function to add two numbers
+
+def add_nums(a, b):
+    return a + b
+
+# Function to subtract two numbers
+def subtract_nums(a, b):
+    return a + b
+
+# Function to multiply two numbers
+def multiply_nums(a, b):
+    return a + b
+
+# Function to divide two numbers
+def divide_nums(a, b):
+    return a + b
+
+add = add_nums(20, 10)
+subtract = subtract_nums(10, 3)
+multiply = multiply_nums(3, 4)
+divide = divide_nums(100, 5)
+
+print """The result of addition, multiplication, substraction and division of two 
+    numbers are: %r %r %r %r""" % (add, multiply, subtract, divide)

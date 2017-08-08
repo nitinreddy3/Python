6c0e74173d27a2a3e6047b95a40d1111d2d57203
Nitin Reddy <reddynitin3@gmail.com>
2017-08-08 16:51:06 +0530

    Example to check the verify the values and the print statements based on corresponding boolean values

---
+++ lpthw/ex29.py
@@ -0,0 +1,30 @@
+# Initialized the variables with values
+people = 20
+cats = 30
+dogs = 15
+
+
+# If statements for comparisons between variable values
+
+if people < cats:
+    print("Too many cats! The world is doomes!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than equal to dogs")
+
+if people <= dogs:
+    print("People are less than equal to dogs")
+
+if people == dogs:
+    print("People are dogs.")

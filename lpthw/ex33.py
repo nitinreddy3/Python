5c46f6f2bbd0ae24c5724c04f5857b8cf5c9aff3
Nitin Reddy <reddynitin3@gmail.com>
2017-08-10 12:48:07 +0530

    Created a function to work with while loop

--- lpthw/ex33.py
+++ lpthw/ex33.py
@@ -1,25 +1,27 @@
 # Program to print the numbers until the condition evaluates to false
-
-i = 0
-
 # Define a empty array
 numbers = []
 
+range_val = int(input("Please enter the range of values you want to operate upon: "))
+incr_val = int(input("Please enter the increment value you want to operate upon: "))
 # A "while" loop to iterate over numbers less than 6 and print the 
 # numbers before increment and after increment
+def go_while_loop(range_value, incr_value):
+    i = 0
+    while i < range_value:
+        print("At the top i is %d" % i)
+        numbers.append(i)
 
-while i < 6:
-    print("At the top i is %d" % i)
-    numbers.append(i)
-
-    i += 1
-    print("Numbers now: ", numbers)
-    print("At the bottom i is %d" % i)
+        i += incr_value
+        print("Numbers now: ", numbers)
+        print("At the bottom i is %d" % i)
 
 print("The numbers: ")
-
 
 # Iterate over the numbers to print the list items
 
 for num in numbers:
     print(num)
+
+go_while_loop(range_val, incr_val)
+

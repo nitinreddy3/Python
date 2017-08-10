0e5e9479c6d7d6bfd53123639efb0fcf4495166c
Nitin Reddy <reddynitin3@gmail.com>
2017-08-10 12:52:06 +0530

    Used a for loop to get rid of while loop and the incrementer

--- lpthw/ex33.py
+++ lpthw/ex33.py
@@ -4,22 +4,20 @@ numbers = []
 
 range_val = int(input("Please enter the range of values you want to operate upon: "))
 incr_val = int(input("Please enter the increment value you want to operate upon: "))
+
 # A "while" loop to iterate over numbers less than 6 and print the 
 # numbers before increment and after increment
 def go_while_loop(range_value, incr_value):
     i = 0
-    while i < range_value:
+    for i in range(0, range_value):
         print("At the top i is %d" % i)
         numbers.append(i)
-
-        i += incr_value
         print("Numbers now: ", numbers)
         print("At the bottom i is %d" % i)
 
 print("The numbers: ")
 
 # Iterate over the numbers to print the list items
-
 for num in numbers:
     print(num)
 

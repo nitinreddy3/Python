d89de7f25905ec375cc91e3f41e5af65aa3db3da
nitin reddy <reddynitin3@gmail.com>
2018-05-07 20:23:51 +0530

    Updated code base in accordance with pep8 recommendations

--- learn-python/helloworld_finished.py
+++ learn-python/helloworld_finished.py
@@ -2,8 +2,10 @@
 # Example file for HelloWorld
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
+
 def main():
-  print ("hello world!")
-    
+    print("hello world!")
+
+
 if __name__ == "__main__":
-  main()
+    main()

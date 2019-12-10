4357820cdfa5cb039e2a4b7efce8c3e717511fd9
Nitin Reddy <reddynitin3@gmail.com>
2019-12-10 13:11:37 +0530

    Simple programs

---
+++ 30days/personInfo.py
@@ -0,0 +1,3 @@
+birth_year = input("Birth year: ")
+age = 2019 - int(birth_year) 
+print("Age: ", age)
---
+++ 30days/weightPerson.py
@@ -0,0 +1,4 @@
+weight_in_pounds = input("What is your weight in pounds?: ")
+print("My current weight is %s pounds", weight_in_pounds)
+weight_in_kg = int(weight_in_pounds) * 0.45
+print("My current weight is %s kgs", weight_in_kg)
